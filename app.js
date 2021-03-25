aa2cafda4891d9eb69d72f80b754a19fa5c42edd
MuzammilRafay <muzammil.rafay@gmail.com>
2021-03-25 23:52:47 +0500

    first class of javascript

---
+++ app.js
@@ -0,0 +1,19 @@
+alert("working");
+
+console.log("this is a console.");
+console.log(12345);
+console.log(null);
+console.log(true);
+console.log(false);
+console.log([1, 2, 3, 4, 5]);
+console.log({ a: 1, b: 2 });
+console.table({ a: 1, b: 2 });
+console.warn("this is a error");
+console.time();
+console.log("hello world");
+console.log("hello world");
+console.log("hello world");
+console.log("hello world");
+console.log("hello world");
+console.log("hello world");
+console.timeEnd();
